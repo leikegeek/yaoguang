ccc49ca5759044f4ad0fb78f3019e4b0b6cdfc41
zhumang <leikegeek@gmail.com>
2021-06-01 22:22:25 +0800

    init project

---
+++ api/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2021/5/29 2:11 下午
+# @Author : 竹芒
+# @Version：V 0.1
+# @File : __init__.py.py
+# @desc :
---
+++ extensions/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2021/5/29 11:30 下午
+# @Author : 竹芒
+# @Version：V 0.1
+# @File : __init__.py.py
+# @desc :
